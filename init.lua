e1c067012bd170beaf89ebb495e1a8d8c83118ea
Nick Saika <nsaika@akamai.com>
2024-04-29 14:56:23 +0000

    Install tree-sitter language for Just

--- init.lua
+++ init.lua
@@ -930,7 +930,11 @@ require('lazy').setup({
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 
-  -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
+
+  -- Add a tree-sitter language for Just (https://just.systems/).
+  { 'IndianBoy42/tree-sitter-just' },
+
+  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
 
