2fa0c7911135c7307b93a295142e20ea09f5e081
Nick Saika <n@nesv.ca>
2024-06-21 00:48:35 -0400

    colorscheme: Add NTBBloodbath/doom-one.nvim, but keep the default theme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,9 +1,54 @@
-return {
-  'slugbyte/lackluster.nvim',
-  lazy = false,
-  priority = 1000,
-  init = function()
-    vim.opt.background = 'dark'
-    vim.cmd.colorscheme 'lackluster'
-  end,
-}
+-- A really nice, mostly-monochrome colour scheme.
+-- return {
+--   'slugbyte/lackluster.nvim',
+--   lazy = false,
+--   priority = 1000,
+--   init = function()
+--     vim.opt.background = 'dark'
+--     vim.cmd.colorscheme 'lackluster'
+--   end,
+-- }
+
+-- ______                      _____
+-- |  _  \                    |  _  |
+-- | | | |___   ___  _ __ ___ | | | |_ __   ___
+-- | | | / _ \ / _ \| '_ ` _ \| | | | '_ \ / _ \
+-- | |/ / (_) | (_) | | | | | \ \_/ / | | |  __/
+-- |___/ \___/ \___/|_| |_| |_|\___/|_| |_|\___|
+--
+-- return {
+--   'NTBBloodbath/doom-one.nvim',
+--   lazy = false,
+--   priority = 1000,
+--   init = function()
+--     vim.g.doom_one_cursor_coloring = false
+--     vim.g.doom_one_terminal_colors = true
+--     vim.g.doom_one_italic_comments = false
+--     vim.g.doom_one_enable_treesitter = true
+--     vim.g.doom_one_diagnostics_text_color = false
+--     vim.g.doom_one_transparent_background = false
+--
+--     -- Pumblend transparency.
+--     vim.g.doom_one_pumblend_enable = false
+--     vim.g.doom_one_pumblend_transparency = 20
+--
+--     -- Plugin integration.
+--     vim.g.doom_one_plugin_neorg = false
+--     vim.g.doom_one_plugin_barbar = false
+--     vim.g.doom_one_plugin_telescope = true
+--     vim.g.doom_one_plugin_neogit = false
+--     vim.g.doom_one_plugin_nvim_tree = false
+--     vim.g.doom_one_plugin_dashboard = false
+--     vim.g.doom_one_plugin_startify = false
+--     vim.g.doom_one_plugin_whichkey = false
+--     vim.g.doom_one_plugin_indent_blankline = false
+--     vim.g.doom_one_plugin_vim_illuminate = false
+--     vim.g.doom_one_plugin_lspsaga = false
+--
+--     vim.opt.background = 'dark'
+--     vim.cmd.colorscheme 'doom-one'
+--   end,
+-- }
+
+-- No colour scheme, because the default neovim 0.10 theme is actually really nice.
+return {}
