85ea0580fa84b28618f4a7d63f21f32544a01680
Nick Saika <n@nesv.ca>
2025-05-07 00:15:10 -0400

    Started from a fresh commit off of master

--- init.lua
+++ init.lua
@@ -144,7 +144,7 @@ vim.opt.splitbelow = true
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
+vim.opt.list = false
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
@@ -886,7 +886,69 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  },
+
+  {
+    'oonamo/ef-themes.nvim',
+    priority = 1000,
+    config = function()
+      require('ef-themes').setup {
+        light = 'ef-spring', -- Ef-theme to select for light backgrounds
+        dark = 'ef-bio', -- Ef-theme to select for dark backgrounds
+        transparent = false,
+        styles = {
+          -- Set specific styles for specific highlight groups
+          -- Can be any valid attr-list value. See `:h nvim_set_hl`
+          comments = { italic = true },
+          keywords = { bold = true },
+          functions = {},
+          variables = {},
+          classes = { bold = true },
+          types = { bold = true },
+
+          diagnostic = 'default', -- Can be "full"
+          pickers = 'default', -- Can be "borderless"
+        },
+
+        modules = {
+          -- Enable/Disable highlights for a module
+          -- See `h: ef-themes-modules` for the list of available modules
+          blink = true,
+          fzf = false,
+          mini = true,
+          semantic_tokens = false,
+          snacks = false,
+          treesitter = true,
+        },
+
+        --- Override any color from the ef-theme
+        ---@param colors Ef-Theme
+        ---@param name string
+        on_colors = function(colors, name) end,
+
+        --- Override specific highlights
+        ---@param highlights table
+        ---@param colors Ef-Theme
+        ---@param name string
+        ---@return table
+        on_highlights = function(highlights, colors, name)
+          -- Returns a table of highlights
+          -- return {
+          --   Normal = { fg = colors.fg_alt, bg = colors.bg_inactive }
+          --   ObscurePlugin = { fg = colors.yellow_faint }
+          -- }
+        end,
+
+        options = {
+          compile = true, -- Whether to compile a theme
+          compile_path = vim.fn.stdpath 'cache' .. '/ef-themes', -- Directory in which to place compiled themes
+        },
+      }
+
+      vim.opt.background = 'dark'
+      vim.cmd.colorscheme 'ef-theme' -- To use the default colorscheme defined above
     end,
   },
 
@@ -925,6 +987,10 @@ require('lazy').setup({
       statusline.section_location = function()
         return '%2l:%-2v'
       end
+
+      -- Enable mini.tabline, which also requires mini.icons.
+      require('mini.icons').setup()
+      require('mini.tabline').setup()
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
